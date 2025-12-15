91719575d5bed2d57df1f4821069358f884fe879
Samir Adhikari <081bct071@ioepc.edu.np>
2025-12-15 07:37:46 +0545

    added routind and made a form submission  page

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const userRouter = require('./routes/user.routes')
 const app = express();
+app.set('view engine','ejs')
 
-app.get('/',(eeq,res)=>{
-    res.send("Hello from page")
-})
+app.use('/user',userRouter)
+
 
 app.listen(3000,()=>{
     console.log("Server Started!!!")
