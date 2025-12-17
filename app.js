fdde6c896a750db315889f5e1dfcc222a80b6cc2
Samir Adhikari <081bct071@ioepc.edu.np>
2025-12-17 19:48:13 +0545

    removed bcrypt , and gone with plain password for continutation

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const app = express();
+
+
+// to show entered form details on console we have to use two middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended:true})) 
+
 const userRouter = require('./routes/user.routes')
+
 
 // requiring dotenv
 const dotenv = require('dotenv');
@@ -8,15 +16,11 @@ dotenv.config();
 // to connect mongoose
 const connectToDB=require('./config/db')
 connectToDB();
-const app = express();
+
 app.set('view engine','ejs')
 
 // to config env for mongoDB
 
-
-// to show entered form details on console we have to use two middlewares
-app.use(express.json());
-app.use(express.urlencoded({extended:true}))
 
 
 app.use('/user',userRouter)
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const { body,validationResult } = require('express-validator');
 const userModel = require('../models/user.model')
 const bcrypt = require('bcrypt');
-// jsonwebtoken is an external commans
+// jsonwebtoken is an external module
+const jwt = require('jsonwebtoken');
 
 router.get('/register',(req,res)=>{
     res.render('register')
@@ -23,17 +24,23 @@ router.post('/register',
         })
     }
 
-    const { email , username , password }= req.body;
+     const { email , username , password }= req.body;
 
-    const hashpassword = await bcrypt.hash(password, 10)
+    //  const hashpassword = await bcrypt.hash(password, 10)
+
+     
 
     const newUser =await  userModel.create({
         email,
         username,
-        password:hashpassword
+        password
+        // password:hashpassword
     })
 
-    res.json(newUser)
+    res.status(201).json({
+  message: "User registered successfully"
+});
+
 })
 
 router.get('/login',(req,res)=>{
@@ -41,8 +48,10 @@ router.get('/login',(req,res)=>{
 })
 
 router.post('/login',
-     body('password').trim().isLength({min:5}),
+    
     body('username').trim().isLength({min:3}),
+     body('password').trim().isLength({min:5}),
+
     async (req,res)=>{
         const errors = validationResult(req);
 
@@ -53,28 +62,48 @@ router.post('/login',
             })
         }
 
-        const {username , password}=req.body;
+        const {username , password} = req.body;
+    
 
-        const user = await userModel.findOneAndUpdate({
+        const user = await userModel.findOne({
+           
             username:username
+            
         })
 
         if(!user){
             return res.status(400).json({
-                messsage:"Username or password is incorrect"
+                messsage:"Username is incorrect"
             })
         }
 
-        const isMatch = await bcrypt.compare(password,user.password)
-
+        // const isMatch = await bcrypt.compare(password,user.password)
+        const isMatch = password === user.password;
         if(!isMatch){
             return res.status(400).json({
-                message:'Username or password is incorrect'
+                message:' password is incorrect'
             })
         }
 
+
+        const token = jwt.sign({
+            userId: user._id,
+            email: user.email,
+            username:user.username
+        },
+    process.env.JWT_SECRET,
+    
+        )
+
+        res.json({
+            token
+        })
 
     }
 )
 
 module.exports = router;
+
+
+
+
