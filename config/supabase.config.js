0eb15efd6bbd0a72fa4c098d7ea8bb5a242852ad
Samir Adhikari <081bct071@ioepc.edu.np>
2025-12-23 09:13:56 +0545

    supabase config

---
+++ config/supabase.config.js
@@ -0,0 +1,15 @@
+const { createClient } = require('@supabase/supabase-js');
+require('dotenv').config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+// Create a single supabase client with admin rights
+const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey, {
+  auth: {
+    autoRefreshToken: false,
+    persistSession: false
+  }
+});
+
+module.exports = supabaseAdmin;
